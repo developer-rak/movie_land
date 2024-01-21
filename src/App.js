46a5b7c27439b10a1d9581eb713a976a328bbd3d
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-01-21 21:20:19 +0500

    movie component

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import searchIcon from './search.svg';
 const API_URL = 'http://www.omdbapi.com?apikey=8025b21b';
 
 const movie1 = {
-    "Title": "Fighting, Flying and Driving: The Stunts of Spiderman 3",
-    "Year": "2007",
-    "imdbID": "tt1132238",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg"
+  "Title": "Spiderman in Cannes",
+  "Year": "2016",
+  "imdbID": "tt5978586",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
 }
 
 const App = () => {
@@ -47,19 +47,7 @@ const App = () => {
       </div>
 
       <div className="container">
-        <div className="movie">
-          
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-          <div>
-            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
-          </div>
-          <div>
-            <span>{movie1.Type}</span>
-          </div>
-
-        </div>
+        
       </div>
     </div>
   );
