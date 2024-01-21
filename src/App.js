a7c3a3d2fd52aa0c0800fdb5ee82b8f0f29e3829
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-01-21 21:59:42 +0500

    taking input search

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ const movie1 = {
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -37,30 +38,29 @@ const App = () => {
       <div className="search">
         <input 
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           />
 
         <img 
           src={searchIcon} 
           alt="search" 
-          onChange={() => {}}
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
-      {
-        movies?.length > 0
-        ? (
-          <div className="container">
-            {movies.map((movie) => (
-              <MovieCard movie={movie}/>
-            ))}
-          </div>
-        ) : (
-          <div className="empty">
-            <h2>No Movies Found</h2>
-          </div>
-        )
+      {movies?.length > 0
+      ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie}/>
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )
       }
 
       
