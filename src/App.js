371541d89c568c546710664363694533f3dd2e9e
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-01-21 13:06:35 +0500

    ...

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data);
+    console.log(data.Search);
   }
 
   useEffect(() => {
