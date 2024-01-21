c940b99b9e96ccc677307ba4b392a26dce347725
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-01-21 21:43:15 +0500

    movies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import './App.css';
 import searchIcon from './search.svg';
+import MovieCard from "./MovieCard";
 
 // 8025b21b
 
@@ -16,16 +17,17 @@ const movie1 = {
 }
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   }
 
   useEffect(() => {
-    searchMovies('spiderman');
+    searchMovies('Avengers');
   }, []);
 
   return (
@@ -46,9 +48,22 @@ const App = () => {
         />
       </div>
 
-      <div className="container">
-        
-      </div>
+      {
+        movies?.length > 0
+        ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie}/>
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No Movies Found</h2>
+          </div>
+        )
+      }
+
+      
     </div>
   );
 }
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
     return (
         <div className="movie">
           
             <div>
-                <p>{movie1.Year}</p>
+                <p>{movie.Year}</p>
             </div>
 
             <div>
-                <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
+                <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
             </div>
         
             <div>
-                <span>{movie1.Type}</span>
-                <h3>{movie1.Title}</h3>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
             </div>
 
         </div>
