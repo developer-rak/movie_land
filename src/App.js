93f9b34ec4cf79d3f653b6b38c572fdbef0c77cd
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-01-21 12:48:32 +0500

    ombd apikey for movies added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from "react";
+import { useEffect } from "react";
+
+// 8025b21b
+
+const API_URL = 'http://www.omdbapi.com?apikey=8025b21b';
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+  useEffect(() => {
+    searchMovies('spiderman');
+  }, []);
+
   return (
     <h1>this is from App</h1>
   );
